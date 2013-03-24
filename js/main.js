82caa861c568915b171ba491dd46e2114143715b
Jérémie Astori <jeremie@astori.fr>
2013-03-24 14:34:52 +0100

    First merge animation/data

--- js/main.js
+++ js/main.js
@@ -21,10 +21,53 @@ function proj(coord){
   return [x * width, y * height]; 
 }
 
+var global_data;
+
+var colors = {
+  none:           { brewer: '',       range: function(d) { return "default"; }}, 
+  jeunesse:       { brewer: 'RdYlGn', range: function(d){return get_brewer_class(d.jeunesse, [1.1, 2, 4]);} },
+  gares :         { brewer: 'Blues',  range: function(d){return get_brewer_class(d.gares,    [10, 40, 80]);} },
+  salaire_median :{ brewer: 'Blues',  range: function(d){return get_brewer_class(d.median,   [15000, 25000, 35000]);} },
+  tx_hlm :        { brewer: 'RdGy',   range: function(d){return get_brewer_class(d.tx_hlm,   [5 ,15, 30]);} },
+  tx_appart :     { brewer: 'RdYlGn', range: function(d){return get_brewer_class(d.tx_appart,[30, 60, 80]);} }
+};
+
+var sizes = {
+  none: {scale: function(x){return 6;}, f: function(d){return 7;} },
+  area: {scale: d3.scale.sqrt().domain([1, 5000]).range([1,10]), f: function(d){return d.surf;} }, 
+  pop99: {scale: d3.scale.pow().domain([1, 250000]).range([1,10]).exponent(0.3), f: function(d){return d.p99_pop;} },
+  pop09: {scale: d3.scale.pow().domain([1, 250000]).range([1,10]).exponent(0.3), f: function(d){return d.p09_pop;} },
+  pop90: {scale: d3.scale.pow().domain([1, 250000]).range([1,10]).exponent(0.3), f: function(d){return d.d90_pop;} },
+  pop82: {scale: d3.scale.pow().domain([1, 250000]).range([1,10]).exponent(0.3), f: function(d){return d.d82_pop;} },
+  pop75: {scale: d3.scale.pow().domain([1, 250000]).range([1,10]).exponent(0.3), f: function(d){return d.d75_pop;} },
+  pop68: {scale: d3.scale.pow().domain([1, 250000]).range([1,10]).exponent(0.3), f: function(d){return d.d68_pop;} },
+};
+
 
 var svg = d3.select("#map").append("svg")
   .attr("width", width)
   .attr("height", height);
+
+function render(color, size){
+  var col = colors[color];
+  var siz = sizes[size];
+  svg.attr("class", col.brewer);
+
+  svg.selectAll("circle")
+  .data(global_data)
+  .transition()
+  .duration("2500")
+  .delay(function(d, i) { return i*2; })
+  .attr("r", function(d){ return siz.scale(parseFloat(siz.f(d)));})
+  .attr("class", col.range);
+}
+
+function get_brewer_class(val, range){
+  if(val < range[0]) return "q0-4";
+  else if(val < range[1])  return "q1-4";
+  else if(val < range[2])  return "q2-4";
+  else return "q3-4";
+}
 
   svg.append("image")
   .attr("xlink:href", "img/idf-830.png")
@@ -33,7 +76,9 @@ var svg = d3.select("#map").append("svg")
   .style("opacity", 0.5);
 
 
-d3.tsv("data/communes.csv", function(error, data) {
+d3.csv("data/communes2.csv", function(error, data) {
+  global_data = data;
+
   data.forEach(function(d) {
     var val = parseFloat(d.Surface);
     coord = proj([d.lon, d.lat]);
@@ -41,9 +86,11 @@ d3.tsv("data/communes.csv", function(error, data) {
     var g = svg.append("g")
       .attr("transform", "translate(" + coord[0] + ", " + (height - coord[1]) + ")");
 
-  g.append("circle")
+  /*g.append("circle")
     .attr("r", Math.sqrt(val)/10)
-    .style("fill", "#6f5f5f");
+    .style("fill", "#6f5f5f");*/
+
+  g.append("circle");
 
   g.append("text")
     .text(d.NomCommune + " - " + val)
@@ -51,6 +98,8 @@ d3.tsv("data/communes.csv", function(error, data) {
     .style("display", "none")
     .style("visibility", "hidden");
   });
+
+  render("none", "none");
 
   d3.selectAll("g")
     .on('mouseover', function(e) {
@@ -112,6 +161,8 @@ $(document).ready(function() {
     $(this).find("img:last-of-type").toggle();
     $(this).find("img:first-of-type").toggle();
     $(this).toggleClass("active");
+
+    render("tx_appart", "pop90");
   });
 
 });
